b4d16d6cd63c94372b42a91d91a856956520e4a5
Roman Balmus <app@tulain.com>
2017-05-06 11:37:47 +0200

    Initial commit

---
+++ RomanNumberCalculator/Dev.swift
@@ -0,0 +1,9 @@
+//
+//  Dev.swift
+//  RomanNumberCalculator
+//
+//  Created by iOS on 06/05/17.
+//  Copyright © 2017 iOS. All rights reserved.
+//
+
+import Foundation
---
+++ RomanNumberCalculator/LoginViewController.swift
@@ -0,0 +1,44 @@
+//
+//  LoginViewController.swift
+//  RomanNumeralCalculator
+//
+//  Created by iOS on 05/05/17.
+//  Copyright © 2017 buddybuild. All rights reserved.
+//
+
+import UIKit
+import Foundation
+//test with unit enabled
+class LoginViewController: UIViewController, FireDBDelegate {
+    
+    @IBOutlet weak var receivedDataLabel: UILabel!
+    var fireDb: FireDB! = nil
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.view.isHidden=false
+        fireDb = FireDB.sharedInstance
+        fireDb.delegate = self
+    }
+    
+    @IBAction func facebookSignInBtnTapped(_ sender: Any) {
+        
+        fireDb.checkDataForNode(node: "objects3")
+    
+    }
+    @IBAction func closeBtnTapped(_ sender: Any) {
+        AppConfig.sharedInstance.userIsLogged=true
+
+        self.dismiss(animated: true) {
+            
+        }
+    }
+    
+    func receivedData(errorString: String, dict: [String : AnyObject]) {
+        if !errorString.isEmpty{
+            receivedDataLabel.text = errorString
+        }else{
+            receivedDataLabel.text = String(describing: dict)
+        }
+    }
+}
